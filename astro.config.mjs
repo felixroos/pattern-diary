70ec16093ef437174cba38730c2b10301d1e596f
Felix Roos <flix91@gmail.com>
2022-12-27 12:51:18 +0100

    add deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  site: "https://felixroos.github.io/pattern-diary",
   integrations: [
     tailwind({
       config: {
